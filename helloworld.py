2731413e3c2777db20b84fe4499015bdbdb58951
smv1999 <vaidhyanathan.sm@gmail.com>
2019-06-10 10:36:27 +0530

    hi

--- helloworld.py
+++
@@ -1,5 +0,0 @@
-#program to print "Hello,World" on the screen
-"""multi-line comments.
-   this shows multiple comments.
-"""
-print("Hello,World!")
--- pythonvariables.py
+++
@@ -1,4 +0,0 @@
-a=10 #integer value assigned to a
-b="hello" #string value assigned to b
-print(a)
-print(b)
